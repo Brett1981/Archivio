1320707170ce2951873d9bbd0813282583fd6372
Stephen Brett <stephenbrett@btinternet.com>
2026-07-21 13:36:37 +0100

    Update EF model snapshot for LibrarySources

--- src/Archivio.Persistence/Migrations/ArchivioDbContextModelSnapshot.cs
+++ src/Archivio.Persistence/Migrations/ArchivioDbContextModelSnapshot.cs
@@ -13,6 +13,20 @@ public partial class ArchivioDbContextModelSnapshot : ModelSnapshot
     {
         modelBuilder.HasAnnotation("ProductVersion", "10.0.10");
 
+        modelBuilder.Entity<LibrarySource>(entity =>
+        {
+            entity.Property(x => x.Id).HasColumnType("TEXT");
+            entity.Property(x => x.CreatedAtUtc).HasColumnType("TEXT");
+            entity.Property(x => x.IsEnabled).HasColumnType("INTEGER");
+            entity.Property(x => x.Name).IsRequired().HasMaxLength(128).HasColumnType("TEXT");
+            entity.Property(x => x.Path).IsRequired().HasMaxLength(1024).HasColumnType("TEXT");
+            entity.Property(x => x.Type).HasConversion<int>().HasColumnType("INTEGER");
+            entity.Property(x => x.UpdatedAtUtc).HasColumnType("TEXT");
+            entity.HasKey(x => x.Id);
+            entity.HasIndex(x => x.Path).IsUnique();
+            entity.ToTable("LibrarySources");
+        });
+
         modelBuilder.Entity<SystemRecord>(entity =>
         {
             entity.Property(x => x.Id).HasColumnType("TEXT");
