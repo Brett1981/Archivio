3a9be46e4bc84cadae0bb2f5e27207b341308d43
Stephen Brett <stephenbrett@btinternet.com>
2026-07-21 15:41:56 +0100

    Support scan progress reporting

--- src/Archivio.Application/Abstractions/ILibraryScanService.cs
+++ src/Archivio.Application/Abstractions/ILibraryScanService.cs
@@ -5,4 +5,9 @@ public interface ILibraryScanService
     Task<LibraryScanResult> ScanAsync(
         Guid librarySourceId,
         CancellationToken cancellationToken = default);
+
+    Task<LibraryScanResult> ScanAsync(
+        Guid librarySourceId,
+        IProgress<LibraryScanProgress>? progress,
+        CancellationToken cancellationToken = default);
 }
