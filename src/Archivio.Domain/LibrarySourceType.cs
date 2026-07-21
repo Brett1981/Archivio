77fbe008501dd3fc6eacc1a2da0bbac9b0972bf4
Stephen Brett <stephenbrett@btinternet.com>
2026-07-21 13:20:41 +0100

    feat(domain): add library source types

---
+++ src/Archivio.Domain/LibrarySourceType.cs
@@ -0,0 +1,12 @@
+namespace Archivio.Domain;
+
+public enum LibrarySourceType
+{
+    Mixed = 0,
+    Movies = 1,
+    Television = 2,
+    Music = 3,
+    Audiobooks = 4,
+    Documents = 5,
+    Photos = 6
+}
