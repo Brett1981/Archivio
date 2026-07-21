85468cbea470c4b15276ddfe00aac94a6edd9d09
Stephen Brett <stephenbrett@btinternet.com>
2026-07-21 15:01:43 +0100

    Add MediaItems migration metadata

---
+++ src/Archivio.Persistence/Migrations/20260721120000_AddMediaItems.Designer.cs
@@ -0,0 +1,71 @@
+using System;
+using Archivio.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Archivio.Persistence.Migrations;
+
+[DbContext(typeof(ArchivioDbContext))]
+[Migration("20260721120000_AddMediaItems")]
+partial class AddMediaItems
+{
+    protected override void BuildTargetModel(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasAnnotation("ProductVersion", "10.0.10");
+
+        modelBuilder.Entity<LibrarySource>(entity =>
+        {
+            entity.Property(x => x.Id).HasColumnType("TEXT");
+            entity.Property(x => x.CreatedAtUtc).HasColumnType("TEXT");
+            entity.Property(x => x.IsEnabled).HasColumnType("INTEGER");
+            entity.Property(x => x.Name).IsRequired().HasMaxLength(128).HasColumnType("TEXT");
+            entity.Property(x => x.Path).IsRequired().HasMaxLength(1024).HasColumnType("TEXT");
+            entity.Property(x => x.Type).HasConversion<int>().HasColumnType("INTEGER");
+            entity.Property(x => x.UpdatedAtUtc).HasColumnType("TEXT");
+            entity.HasKey(x => x.Id);
+            entity.HasIndex(x => x.Path).IsUnique();
+            entity.ToTable("LibrarySources");
+        });
+
+        modelBuilder.Entity<MediaItem>(entity =>
+        {
+            entity.Property(x => x.Id).HasColumnType("TEXT");
+            entity.Property(x => x.ContentHash).HasMaxLength(128).HasColumnType("TEXT");
+            entity.Property(x => x.CreatedAtUtc).HasColumnType("TEXT");
+            entity.Property(x => x.Extension).IsRequired().HasMaxLength(32).HasColumnType("TEXT");
+            entity.Property(x => x.FileName).IsRequired().HasMaxLength(512).HasColumnType("TEXT");
+            entity.Property(x => x.FullPath).IsRequired().HasMaxLength(2048).HasColumnType("TEXT");
+            entity.Property(x => x.IsMissing).HasColumnType("INTEGER");
+            entity.Property(x => x.LastScannedAtUtc).HasColumnType("TEXT");
+            entity.Property(x => x.LibrarySourceId).HasColumnType("TEXT");
+            entity.Property(x => x.ModifiedAtUtc).HasColumnType("TEXT");
+            entity.Property(x => x.RelativePath).IsRequired().HasMaxLength(2048).HasColumnType("TEXT");
+            entity.Property(x => x.SizeBytes).HasColumnType("INTEGER");
+            entity.HasKey(x => x.Id);
+            entity.HasIndex(x => new { x.LibrarySourceId, x.FullPath }).IsUnique();
+            entity.HasIndex(x => new { x.LibrarySourceId, x.RelativePath });
+            entity.ToTable("MediaItems");
+        });
+
+        modelBuilder.Entity<SystemRecord>(entity =>
+        {
+            entity.Property(x => x.Id).HasColumnType("TEXT");
+            entity.Property(x => x.CreatedAtUtc).HasColumnType("TEXT");
+            entity.Property(x => x.Name).IsRequired().HasMaxLength(128).HasColumnType("TEXT");
+            entity.Property(x => x.Value).IsRequired().HasMaxLength(1024).HasColumnType("TEXT");
+            entity.HasKey(x => x.Id);
+            entity.HasIndex(x => x.Name).IsUnique();
+            entity.ToTable("SystemRecords");
+        });
+
+        modelBuilder.Entity<MediaItem>()
+            .HasOne(x => x.LibrarySource)
+            .WithMany()
+            .HasForeignKey(x => x.LibrarySourceId)
+            .OnDelete(DeleteBehavior.Cascade)
+            .IsRequired();
+    }
+}
