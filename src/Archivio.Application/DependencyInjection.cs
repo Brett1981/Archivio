a29847e823e45bc20db4cd24cc0ee0ff4bb3cc4a
Stephen Brett <stephenbrett@btinternet.com>
2026-07-21 14:04:35 +0100

    Register library source application service

--- src/Archivio.Application/DependencyInjection.cs
+++ src/Archivio.Application/DependencyInjection.cs
@@ -1,4 +1,6 @@
+using Archivio.Application.Abstractions;
 using Archivio.Application.Configuration;
+using Archivio.Application.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,6 +14,8 @@ public static class DependencyInjection
             .Bind(configuration.GetSection(ArchivioOptions.SectionName))
             .ValidateDataAnnotations()
             .ValidateOnStart();
+
+        services.AddScoped<ILibrarySourceService, LibrarySourceService>();
 
         return services;
     }
