20d9a2e9d9dcb00369091d3586d374c7e52157c4
Stephen Brett <stephenbrett@btinternet.com>
2026-07-21 13:21:09 +0100

    feat(persistence): map library sources

--- src/Archivio.Persistence/ArchivioDbContext.cs
+++ src/Archivio.Persistence/ArchivioDbContext.cs
@@ -6,6 +6,7 @@ namespace Archivio.Persistence;
 public sealed class ArchivioDbContext(DbContextOptions<ArchivioDbContext> options) : DbContext(options)
 {
     public DbSet<SystemRecord> SystemRecords => Set<SystemRecord>();
+    public DbSet<LibrarySource> LibrarySources => Set<LibrarySource>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -16,6 +17,19 @@ public sealed class ArchivioDbContext(DbContextOptions<ArchivioDbContext> option
             entity.Property(x => x.Name).HasMaxLength(128).IsRequired();
             entity.Property(x => x.Value).HasMaxLength(1024).IsRequired();
             entity.HasIndex(x => x.Name).IsUnique();
+        });
+
+        modelBuilder.Entity<LibrarySource>(entity =>
+        {
+            entity.ToTable("LibrarySources");
+            entity.HasKey(x => x.Id);
+            entity.Property(x => x.Name).HasMaxLength(128).IsRequired();
+            entity.Property(x => x.Path).HasMaxLength(1024).IsRequired();
+            entity.Property(x => x.Type).HasConversion<int>().IsRequired();
+            entity.Property(x => x.IsEnabled).IsRequired();
+            entity.Property(x => x.CreatedAtUtc).IsRequired();
+            entity.Property(x => x.UpdatedAtUtc).IsRequired();
+            entity.HasIndex(x => x.Path).IsUnique();
         });
     }
 }
