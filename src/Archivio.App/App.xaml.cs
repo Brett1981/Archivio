85526a4d7649fa952bdb09a26bb5697d67413773
Stephen Brett <stephenbrett@btinternet.com>
2026-07-21 14:32:03 +0100

    Register media services in application host

--- src/Archivio.App/App.xaml.cs
+++ src/Archivio.App/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Windows;
 using Archivio.Application;
 using Archivio.Application.Abstractions;
 using Archivio.Infrastructure;
+using Archivio.Media;
 using Archivio.Persistence;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -44,6 +45,7 @@ public partial class App : System.Windows.Application
                     services.AddApplication(context.Configuration);
                     services.AddInfrastructure();
                     services.AddPersistence();
+                    services.AddMedia();
                     services.AddSingleton<IFolderPickerService, FolderPickerService>();
                     services.AddSingleton<MainWindowViewModel>();
                     services.AddSingleton<MainWindow>();
